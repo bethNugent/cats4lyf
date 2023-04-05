9eca2280d942ea0db62a7b715da5efe2c14913e4
ApacheTech Solutions <p_matthews@live.co.uk>
2023-04-05 17:29:09 +0100

    Add `gh-pages` as dev dependency.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
 import Checkout from './pages/Checkout';
 import About from './pages/About';
 
-import './App.css';
 import HeaderBar from './components/HeaderBar';
+
+import './App.css';
 
 export default function App()
 {
     return (
         <div className="App">
-            <BrowserRouter>
+            <BrowserRouter basename={ (process.env.NODE_ENV === 'development') ? "/" : "/cats4lyf" }>
                 <HeaderBar />
                 <Routes>
                     <Route path="/" element={<HomePage />} />
