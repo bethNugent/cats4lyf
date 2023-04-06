43c809027bed4ca3749edcab131235427d932048
ApacheTech Solutions <p_matthews@live.co.uk>
2023-04-06 12:44:16 +0100

    Add footer links to `Footer` component.

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import '../layout.css';
 import './Footer.css';
 
 /**
@@ -8,7 +8,14 @@ import './Footer.css';
  */
 export default function Footer()
 {
-    return(<footer className='footer-container'>
-
+    return (<footer className='footer-container'>
+        <nav className='nav-section'>
+            <ul>
+                <li><a className="navLink" href="https://www.wwf.org.uk" target="_blank">WWF</a></li>
+                <li><a className="navLink" href="https://www.rspca.org.uk" target="_blank">RSPCA</a></li>
+                <li><a className="navLink" href="https://adch.org.uk" target="_blank">ADCH</a></li>
+                <li><a className="navLink" href="https://www.cats.org.uk" target="_blank">CATS PROTECTION</a></li>
+            </ul>
+        </nav>
     </footer>);
 }
