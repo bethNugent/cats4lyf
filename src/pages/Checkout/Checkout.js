31dde7a1e6ed5ac7a81ab5f1396d11032481f360
Migs9 <galleuk@gmail.com>
2023-04-06 09:59:48 +0100

    updated checkout page

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -1,8 +1,128 @@
+import React, { useState, useEffect } from "react";
+import Modal from 'react-modal';
 import './Checkout.css';
 
-export default function Checkout() {
+function Checkout() {
+  const [cartItems, setCartItems] = useState([
+      {
+        id: 1,
+        name: "Fluffy",
+        breed: 'Siamese',
+        age: '2yrs',
+        donation: 0
+      },
+      {
+        id: 2,
+        name: "Whiskers",
+        breed: 'Bengal',
+        age: '8yrs',
+        donation: 0
+      }
+  ]);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [showModal, setShowModal] = useState(false);
 
-    return(<div className="checkout-container">
+  useEffect(() => {
+    const cartData = localStorage.getItem("cartData");
+    if (cartData) {
+      const parsedData = JSON.parse(cartData);
+      setCartItems(parsedData.cartItems);
+      setTotalPrice(parsedData.totalPrice);
+    }
+  }, []);
 
-    </div>);
+  const removeItem = (itemId) => {
+    const updatedCartItems = cartItems.filter((item) => item.id !== itemId);
+    const updatedTotalPrice = calculateTotalPrice(updatedCartItems);
+    setCartItems(updatedCartItems);
+    setTotalPrice(updatedTotalPrice);
+    updateLocalStorage(updatedCartItems, updatedTotalPrice);
+  };
+
+  const updateDonation = (itemId, value) => {
+    const updatedCartItems = cartItems.map((item) => {
+      if (item.id === itemId) {
+        return { ...item, donation: parseFloat(value) || 0 };
+      }
+      return item;
+    });
+    const updatedTotalPrice = calculateTotalPrice(updatedCartItems);
+    setCartItems(updatedCartItems);
+    setTotalPrice(updatedTotalPrice);
+    updateLocalStorage(updatedCartItems, updatedTotalPrice);
+  };
+
+  const calculateTotalPrice = (items) => {
+    let totalPrice = 0;
+    items.forEach((item) => {
+      totalPrice += item.donation;
+    });
+    return totalPrice;
+  };
+
+  const updateLocalStorage = (cartItems, totalPrice) => {
+    const cartData = { cartItems, totalPrice };
+    localStorage.setItem("cartData", JSON.stringify(cartData));
+  };
+
+  const clearCart = () => {
+    localStorage.removeItem("cartData");
+    setCartItems([]);
+    setTotalPrice(0);
+  };
+
+  return (
+    <div className="checkout-container">
+      <h1>Adoption Details</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Cat Name</th>
+            <th>Breed</th>
+            <th>Age</th>
+            <th>Donation</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cartItems.map((item) => (
+            <tr key={item.id}>
+              <td>{item.name}</td>
+              <td>{item.breed}</td>
+              <td>{item.age}</td>
+              <td>
+                <input type='number' value={item.donation} onChange={(e) => updateDonation(item.id, e.target.value)} onFocus={(e) => e.target.select()}/>
+              </td>
+              <td>
+                <button onClick={() => removeItem(item.id)}>
+                  Remove
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+          <td colSpan="5">
+            <button onClick={clearCart}>Clear Cart</button>
+          </td>
+
+          </tr>
+        </tfoot>
+      </table>
+          <p className="price-checkout">Total Price:  {totalPrice}</p>
+          <button onClick={() => cartItems.length > 0 ? setShowModal(true) : alert('Your cart is empty')} className='checkout'>Checkout</button>
+          <Modal isOpen={showModal} onRequestClose={() => { setShowModal(false); clearCart(); }} 
+          className='modal'
+          style={{
+            overlay: {
+              backgroundColor: "rgba(0, 0, 0, 0.4)",
+          }}}>
+            <button onClick={() => { setShowModal(false); clearCart(); }}>X</button>
+            <h2>Thanks for adopting!</h2>
+            <img src="https://i.giphy.com/media/MDJ9IbxxvDUQM/giphy.webp" alt="cat-hug"/>
+          </Modal>
+    </div>
+  );
 }
+
+export default Checkout;
