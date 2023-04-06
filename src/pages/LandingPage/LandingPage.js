134cf5484b70936adc5b75d3bfd1e4bc54bbf603
ApacheTech Solutions <p_matthews@live.co.uk>
2023-04-06 15:08:15 +0100

    Add leaderboard image

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,8 +1,12 @@
 import './LandingPage.css';
 
+import leaderboardImage from './images/leaderboard.png';
+
 export default function LandingPage() {
 
     return(<div className="landing-container">
-
+        <section className='leaderboard'>
+            <img src={leaderboardImage} alt="Paws For Thought" title="Paws For Thought" />
+        </section>
     </div>);
 }
