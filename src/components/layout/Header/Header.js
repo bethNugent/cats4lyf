b23bed874ea9684415d45ea0f04d047211eae216
ApacheTech Solutions <p_matthews@live.co.uk>
2023-04-06 11:54:07 +0100

    Add logo section to `Header` component.

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -3,6 +3,9 @@ import './Header.css';
 export default function Header()
 {
     return(<header className='header-container'>
-
+        <section className='logo-section'>
+            <p className='title'>Catz-4-Lyf</p>
+            <p className='subtitle'>Adoption Agency</p>
+        </section>
     </header>);
 }
